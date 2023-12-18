0a2f31c3696aa6ac6352c8422443e17cd9de3afe
conghaowong <conghaowong@icloud.com>
2023-12-18 10:24:01 +0800

    Add scripts (from Project-Jocker)

---
+++ scripts/update_readme.py
@@ -0,0 +1,23 @@
+"""
+@Author: Conghao Wong
+@Date: 2021-08-05 15:26:57
+@LastEditors: Conghao Wong
+@LastEditTime: 2023-12-18 10:23:18
+@Description: file content
+@Github: https://github.com/cocoon2wong
+@Copyright 2022 Conghao Wong, All Rights Reserved.
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('.'))
+
+import qpid
+from qpid.mods import vis
+
+TARGET_FILE = './README.md'
+
+
+if __name__ == '__main__':
+    qpid.help.update_readme(qpid.print_help_info(), TARGET_FILE)
