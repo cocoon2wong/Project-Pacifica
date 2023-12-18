11819d5395dcd6c5c8b7ac2bcaedaf21df52a624
conghaowong <conghaowong@icloud.com>
2023-12-18 11:28:16 +0800

    Update qpid version

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 @Author: Conghao Wong
 @Date: 2022-06-20 15:28:14
 @LastEditors: Conghao Wong
-@LastEditTime: 2023-12-18 10:24:26
+@LastEditTime: 2023-12-18 11:17:06
 @Description: file content
 @Github: https://github.com/cocoon2wong
 @Copyright 2022 Conghao Wong, All Rights Reserved.
@@ -13,34 +13,23 @@ import sys
 import torch
 
 import qpid
-from scripts.utils import get_value
 
 torch.autograd.set_detect_anomaly(True)
 
 
 def main(args: list[str], run_train_or_test=True):
-    h_value = None
 
-    if '--help' in args:
-        h_value = get_value('--help', args, default='all_args')
-    elif '-h' in args:
-        h_value = get_value('-h', args, default='all_args')
-
-    if h_value:
-        from qpid.mods import vis
-        qpid.print_help_info(h_value)
+    min_args = qpid.args.Args(args, is_temporary=True)
+    if min_args.help != 'null':
+        qpid.print_help_info('all_args')
         exit()
 
-    min_args = qpid.args.Args(terminal_args=args,
-                              is_temporary=True)
-
-    model = min_args.model
-    if model == 'linear':
+    if (model := min_args.model) == 'linear':
         s = qpid.applications.Linear
     else:
         s = qpid.silverballers.SILVERBALLERS_DICT.get_structure(model)
 
-    t = s(terminal_args=args)
+    t = s(args)
 
     if run_train_or_test:
         t.train_or_test()
